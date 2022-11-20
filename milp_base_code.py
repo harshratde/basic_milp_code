126ed74887bf64b58cfddd3b5374926b7e1dc95b
HarshRatde <harshratde8@gmail.com>
2022-11-20 05:19:18 +0400

    added two more descision variables

--- milp_base_code.py
+++ milp_base_code.py
@@ -16,7 +16,7 @@ print('DATA PREP STARTED')
 
 data = pd.DataFrame({'cols': ['a','a', 'b','b','b', 'c', 'c', 'c', 'c']
                     , 'key_col' : [f'c_{i}'for i in range(9)]
-                     , 'val': [2,1,3,4,5,6,1,8,9]
+                     , 'val': [20,10,30,40,50,60,10,80,90]
                     })
 
 base_data = data.copy()
@@ -24,12 +24,19 @@ master_key = ['cols', 'key_col']
 data.set_index(master_key , inplace=True)
 
 print(data)
+cols_list = ['cols']
+cols_df = base_data[cols_list].drop_duplicates()
+cols_df.set_index(cols_list, inplace=True)
 
-cols_list = base_data['cols'].unique().tolist()
-key_cols_list = base_data['key_col'].unique().tolist()
 
+key_cols_list = ['key_col']
+key_cols_df = base_data[key_cols_list].drop_duplicates()
+key_cols_df.set_index(key_cols_list, inplace=True)
 
 tm_key_set = data.index.unique()
+
+
+cols_list_df = pd.DataFrame({'':cols_list})
 
 # =============================================================================
 # 
@@ -41,27 +48,48 @@ m = pe.ConcreteModel()
 
 m.input_df      = pe.Set(initialize = tm_key_set , dimen = len(master_key))
 
-m.cols  = pe.Set(initialize = cols_list)
-m.key_cols  = pe.Set(initialize = key_cols_list)
+m.cols  = pe.Set(initialize = cols_df.index.unique())
+m.key_cols  = pe.Set(initialize = key_cols_df.index.unique())
 # =============================================================================
 # 
 # =============================================================================
 
 
-m.Y     = pe.Var(m.input_df, domain=pe.NonNegativeIntegers,bounds=(0,2))
+m.Y      = pe.Var(m.input_df, domain=pe.NonNegativeIntegers,bounds=(0,2))
+m.UB     = pe.Var(m.cols, domain=pe.NonNegativeIntegers,bounds=(1,100))
+m.LB     = pe.Var(m.cols, domain=pe.NonNegativeIntegers,bounds=(1,100))
 
-# def obj_rule(m):
-#     return (sum([((m.Y[e]*(data.loc[e, 'val']))-10)**2  for e in tm_key_set]))
+dict_dec_var = {'Y':tm_key_set
+               , 'UB': cols_df.index.unique()
+               , 'LB': cols_df.index.unique()}
+key_dec_var = {'Y':master_key
+               , 'UB': cols_list
+               , 'LB': cols_df.index.unique()}
+
+
+
 def obj_rule(m):
-    return (sum([m.Y[e]*data.loc[e, 'val']  for e in tm_key_set]))
+    return (sum([((m.Y[e]*(data.loc[e, 'val']))-10)**2  for e in tm_key_set]))
+# def obj_rule(m):
+#     return (sum([m.Y[e] for e in tm_key_set]))
 
 m.OBJ = pe.Objective(rule=obj_rule, sense=pe.minimize)
-
 
 
 def impact_push_show_l(m,j):
     return sum([m.Y[e] for e in tm_key_set if j == e[master_key.index('cols')]])==1    
 m.impact_push_show_cl = pe.Constraint(m.cols, rule = impact_push_show_l)
+
+def const_lb(m,j):
+    return sum([m.Y[e]*(data.loc[e, 'val']) for e in tm_key_set if j == e[master_key.index('cols')]])*2== m.LB[j]    
+m.const_lb = pe.Constraint(m.cols, rule = const_lb)
+
+def const_ub(m,j):
+    return sum([m.Y[e]*(data.loc[e, 'val']) for e in tm_key_set if j == e[master_key.index('cols')]])== m.UB[j]    
+m.const_ub = pe.Constraint(m.cols, rule = const_ub)
+
+
+
 
 # =============================================================================
 # 
@@ -71,9 +99,9 @@ print('\n---------------------------------------------')
 print('OPTIMIZATION SOLVER START')
 
 # opt = pyo.SolverFactory('baron')
-opt = pe.SolverFactory('glpk')
+opt = pe.SolverFactory('gurobi')
 
-opt.solve(m) 
+opt.solve(m, keepfiles = True, tee=True) 
 
 
 dec_var = [str(v) for v in m.component_objects(Var,active=True)]
@@ -89,8 +117,11 @@ for v in dec_var:
     print(v)
     print(varobject)
         
-    opt_out = {}    
-    key_id = list(tm_key_set.names)
+    opt_out = {}
+    
+    ref_list = dict_dec_var[v]
+    key_id = list(ref_list.names)
+    print(key_id)
     
     for id_num in range(0,len(key_id)):
         globals()['key_list_{}'.format(id_num)] =[]
@@ -108,9 +139,14 @@ for v in dec_var:
     opt_out['value'] = value_list
     
     globals()['opt_out_df_'+v] = pd.DataFrame(opt_out)
-    globals()['opt_out_df_'+v].set_index(master_key, inplace=True)
+    globals()['opt_out_df_'+v].set_index(ref_list.names, inplace=True)
     globals()['opt_out_df_'+v].sort_index(inplace=True)
 
+opt_out_df_Y
+opt_out_df_LB
+opt_out_df_UB
+    
+    
 #==============================================================================
 #
 #    OPTIMIZATION COMPLETE THE CORRESPONIND DECISION VARIABLE COMPUTATION
